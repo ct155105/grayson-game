3b6afe42fcc7bfcfc19a42ef7e2a87cfef981821
Chris Teuschler <christeuschler@Laurens-Air.attlocal.net>
2023-04-08 00:11:57 -0400

    Game board rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "@/grayson-game/grayson-game.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/hello-world.js
+++ src/pages/hello-world.js
@@ -1,3 +1,9 @@
+import Script from "next/script";
+
 export default function HelloWorld({ Component, pageProps }) {
-  return <h1>Hello World</h1>;
+  // return <h1>Hello World</h1>;
+  return <div>
+  <h1>HelloWorld</h1>
+    <Script src="/game.js" />
+  </div>;
 }
